86b5bf627f72dc22b206855729144f4df24da845
Raymon Sutedjo-The <raymon.sutedjothe@gmail.com>
2018-07-05 02:07:40 -0700

    php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="_css/style.css">
+		<link href="https://fonts.googleapis.com/css?family=Rubik:300i,400,500,900" rel="stylesheet">
+		<title>Raymon Sutedjo-The</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+	</head>
+
+	<body>
+		<main>
+			<h1>Hi</h1>
+		</main>
+	</body>
+
+</html>
