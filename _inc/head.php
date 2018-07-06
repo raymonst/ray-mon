5608ddf111f0cb2265bb83883dddf96baa0d93e6
Raymon Sutedjo-The <raymon.sutedjothe@gmail.com>
2018-07-06 00:47:44 -0700

    Added php + css animations + sass

---
+++ _inc/head.php
@@ -0,0 +1,3 @@
+		<link rel="stylesheet" type="text/css" href="_css/style.css">
+		<link href="https://fonts.googleapis.com/css?family=Rubik:300i,400,500,900" rel="stylesheet">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
---
+++ portfolio.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php include "_inc/head.php" ?>
+		<title>Raymon Sutedjo-The - Portfolio</title>
+	</head>
+
+	<body>
+		<main>
+			<h1>PORTFOLIO</h1>
+			<ul class="list-item-visual">
+				<li>
+					<figure><a href="portfolio-conversational-analytics.php"><img src="_images/placeholder-1.jpg" alt="" width="800" height="600"></a></figure>
+					<div class="list-item-visual-title"><span><a href="portfolio-conversational-analytics.php">Conversational Analytics</a></span></div>
+					<div class="list-item-visual-description"><span>Enabling conversations with your data</span></div>
+				</li>
+				<li>
+					<figure><a href="portfolio-lightning-messaging-framework.php"><img src="_images/placeholder-2.jpg" alt="" width="800" height="600"></a></figure>
+					<div class="list-item-visual-title"><span><a href="#">Lightning Messaging Framework</a></span></div>
+					<div class="list-item-visual-description"><span>Moving a design system forward</span></div>
+				</li>
+				<li>
+					<figure><a href="portfolio-lightning-messaging-framework.php"><img src="_images/placeholder-3.jpg" alt="" width="800" height="600"></a></figure>
+					<div class="list-item-visual-title"><span><a href="#">Analytics Learning App</a></span></div>
+					<div class="list-item-visual-description"><span>Educating users with hands-on examples &amp; best practices
+</span></div	>
+				</li>
+			</ul>
+
+			<h2>OTHER WORKS</h2>
+			<hr class="separator" />
+			<ul class="list-item-visual list-item-visual-s">
+				<li>
+					<figure><a href="portfolio-conversational-analytics.php"><img src="_images/placeholder-4.jpg" alt="" width="800" height="600"></a></figure>
+					<div class="list-item-visual-title"><span><a href="portfolio-conversational-analytics.php">Lorem ipsum</a></span></div>
+					<div class="list-item-visual-description"><span>Nulla placerat ante quis bibendum pretium ut neque sem.</span></div>
+				</li>
+				<li>
+					<figure><a href="portfolio-conversational-analytics.php"><img src="_images/placeholder-4.jpg" alt="" width="800" height="600"></a></figure>
+					<div class="list-item-visual-title"><span><a href="portfolio-conversational-analytics.php">Lorem ipsum</a></span></div>
+					<div class="list-item-visual-description"><span>Nulla placerat ante quis bibendum pretium ut neque sem.</span></div>
+				</li>
+				<li>
+					<figure><a href="portfolio-conversational-analytics.php"><img src="_images/placeholder-4.jpg" alt="" width="800" height="600"></a></figure>
+					<div class="list-item-visual-title"><span><a href="portfolio-conversational-analytics.php">Lorem ipsum</a></span></div>
+					<div class="list-item-visual-description"><span>Nulla placerat ante quis bibendum pretium ut neque sem.</span></div>
+				</li>
+				<li>
+					<figure><a href="portfolio-conversational-analytics.php"><img src="_images/placeholder-4.jpg" alt="" width="800" height="600"></a></figure>
+					<div class="list-item-visual-title"><span><a href="portfolio-conversational-analytics.php">Lorem ipsum</a></span></div>
+					<div class="list-item-visual-description"><span>Nulla placerat ante quis bibendum pretium ut neque sem.</span></div>
+				</li>
+				<li>
+					<figure><a href="portfolio-conversational-analytics.php"><img src="_images/placeholder-4.jpg" alt="" width="800" height="600"></a></figure>
+					<div class="list-item-visual-title"><span><a href="portfolio-conversational-analytics.php">Lorem ipsum</a></span></div>
+					<div class="list-item-visual-description"><span>Nulla placerat ante quis bibendum pretium ut neque sem.</span></div>
+				</li>
+			</div>
+
+		</main>
+	</body>
+
+</html>
